2f69903fac37ecf02d51c57c3d1e6b94969aaf8b
jfqd <jfqd@blun.org>
2012-12-01 09:18:25 +0100

    let method_missing do its work

--- lib/mail_handler_patch.rb
+++ lib/mail_handler_patch.rb
@@ -13,30 +13,21 @@ module RedmineHelpdesk
       # Overrides the dispatch_to_default method
       def dispatch_to_default_with_helpdesk
         issue = receive_issue
-        begin
-          # add owner-email only if the email is comming from an AnonymousUser
-          if issue.author.class == AnonymousUser
-            sender_email = @email.from.to_a.first.to_s.strip
-            custom_field = CustomField.find_by_name('owner-email')
-            custom_value = CustomValue.find(
-              :first,
-              :conditions => ["customized_id = ? AND custom_field_id = ?", issue.id, custom_field.id]
-            )
-            custom_value.value = sender_email
-            custom_value.save(false) # skip validation!
-            # regular email sending to known users is done
-            # on the first issue.save. So we need to send
-            # the notification email to the supportclient
-            # on our own.
-            Mailer.deliver_email_to_supportclient(issue, sender_email)
-          end
-        rescue Exception => e
-          msg = ("The following error occured while sending email notification: Failed to set owner-email \"#{e.message}\". Hint: is the owner-email part of the project?")
-          if logger && logger.error
-            logger.error(msg)
-          else
-            $stderr.puts(msg)
-          end
+        # add owner-email only if the email is comming from an AnonymousUser
+        if issue.author.class == AnonymousUser
+          sender_email = @email.from.to_a.first.to_s.strip
+          custom_field = CustomField.find_by_name('owner-email')
+          custom_value = CustomValue.find(
+            :first,
+            :conditions => ["customized_id = ? AND custom_field_id = ?", issue.id, custom_field.id]
+          )
+          custom_value.value = sender_email
+          custom_value.save(false) # skip validation!
+          # regular email sending to known users is done
+          # on the first issue.save. So we need to send
+          # the notification email to the supportclient
+          # on our own.
+          Mailer.deliver_email_to_supportclient(issue, sender_email)
         end
         issue
       end
