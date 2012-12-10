80576df64d925c9b8836953da8b6c07444b284e0
jfqd <jfqd@blun.org>
2012-12-10 13:20:01 +0100

    add a fallback if no email-footer is available

--- lib/mailer_patch.rb
+++ lib/mailer_patch.rb
@@ -2,9 +2,54 @@ module RedmineHelpdesk
   module MailerPatch
     def self.included(base) # :nodoc:
       base.send(:include, InstanceMethods)
+      
+      base.class_eval do
+        alias_method_chain :issue_edit,  :helpdesk
+      end
     end
-    
+
     module InstanceMethods
+      # Overrides the issue_edit method which is only
+      # be called on existing tickets. We will add the
+      # owner-email to the recipients only if no email-
+      # footer text is available.
+      def issue_edit_with_helpdesk(journal)
+        issue = journal.journalized.reload
+        redmine_headers 'Project' => issue.project.identifier,
+                        'Issue-Id' => issue.id,
+                        'Issue-Author' => issue.author.login
+        redmine_headers 'Issue-Assignee' => issue.assigned_to.login if issue.assigned_to
+        message_id journal
+        references issue
+        @author = journal.user
+        all_recipients = issue.recipients
+        # add owner-email to the recipients
+        begin
+          if journal.send_to_owner == true
+            f = CustomField.find_by_name('helpdesk-email-footer')
+            p = issue.project
+            owner_email = issue.custom_value_for( CustomField.find_by_name('owner-email') ).value
+            if !owner_email.blank? && !f.nil? && !p.nil? && !p.custom_value_for(f).try(:value).blank?
+              all_recipients << owner_email
+            end
+          end
+        rescue Exception => e
+          mylogger.error "Error while adding owner-email to recipients of email notification: \"#{e.message}\"."
+        end
+        recipients all_recipients
+        # Watchers in cc
+        cc(issue.watcher_recipients - @recipients)
+        s = "[#{issue.project.name} - #{issue.tracker.name} ##{issue.id}] "
+        s << "(#{issue.status.name}) " if journal.new_value_for('status_id')
+        s << issue.subject
+        subject s
+        body :issue => issue,
+             :journal => journal,
+             :issue_url => url_for(:controller => 'issues', :action => 'show', :id => issue)
+
+        render_multipart('issue_edit', body)
+      end
+      
       # sending email notifications to the supportclient
       def email_to_supportclient(issue, sender_email, text='')
         redmine_headers 'Project' => issue.project.identifier,
