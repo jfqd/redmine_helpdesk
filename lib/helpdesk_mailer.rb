3d02786fae56034ecf311f0be6e914547b22a532
jfqd <jfqd@blun.org>
2013-06-18 17:41:29 +0200

    with redmine 2.3 we need our own mailer class

---
+++ lib/helpdesk_mailer.rb
@@ -0,0 +1,71 @@
+
+class HelpdeskMailer < ActionMailer::Base
+  # Sending email notifications to the supportclient
+  def email_to_supportclient(issue, recipient, journal=nil, text='')
+    redmine_headers 'Project' => issue.project.identifier,
+                    'Issue-Id' => issue.id,
+                    'Issue-Author' => issue.author.login
+    redmine_headers 'Issue-Assignee' => issue.assigned_to.login if issue.assigned_to
+    message_id issue
+    subject = "[#{issue.project.name} - #{issue.tracker.name} ##{issue.id}] (#{issue.status.name}) #{issue.subject}"
+    # Set 'from' email-address to 'helpdesk-sender-email' if available.
+    # Falls back to regular redmine behaviour if 'sender' is empty.
+    p = issue.project
+    s = CustomField.find_by_name('helpdesk-sender-email')
+    sender = if !p.nil? && !s.nil?
+      p.custom_value_for(s).try(:value)
+    end
+    # If a custom field with text for the first reply is
+    # available then use this one instead of the regular
+    r = CustomField.find_by_name('helpdesk-first-reply')
+    f = CustomField.find_by_name('helpdesk-email-footer')
+    reply  = p.nil? || r.nil? ? '' : p.custom_value_for(r).try(:value)
+    footer = p.nil? || f.nil? ? '' : p.custom_value_for(f).try(:value)
+    # create mail object to deliver
+    mail = if text.present?
+      # sending out the journal note to the support client
+      mail(
+        :from    => sender || Setting.mail_from,
+        :to      => recipient,
+        :subject => subject,
+        :body    => "#{text}\n\n#{footer}".gsub("##issue-id##", issue.id.to_s),
+        :date    => Time.zone.now
+      )
+    elsif reply.present?
+      # sending out the first reply message
+      mail(
+        :from    => sender || Setting.mail_from,
+        :to      => recipient,
+        :subject => subject,
+        :body    => "#{reply}\n\n#{footer}".gsub("##issue-id##", issue.id.to_s),
+        :date    => Time.zone.now
+      )
+    else
+      # fallback to a regular notifications email with redmine view
+      @issue = issue
+      @journal = journal
+      @issue_url = url_for(:controller => 'issues', :action => 'show', :id => issue)
+      mail(
+        :from    => sender || Setting.mail_from,
+        :to      => recipient,
+        :subject => subject,
+        :date    => Time.zone.now,
+        :template_path => 'mailer',
+        :template_name => 'issue_edit'
+      )
+    end
+    return mail
+  end
+
+  private
+
+  # Appends a Redmine header field (name is prepended with 'X-Redmine-')
+  def redmine_headers(h)
+    h.each { |k,v| headers["X-Redmine-#{k}"] = v.to_s }
+  end
+
+  def message_id(object)
+    @message_id_object = object
+  end
+  
+end
--- lib/mailer_patch.rb
+++ lib/mailer_patch.rb
@@ -38,7 +38,8 @@ module RedmineHelpdesk
         end
         recipients = all_recipients
         # Watchers in cc
-        cc = journal.watcher_recipients - @recipients
+        watchers = journal.watcher_recipients || []
+        cc = watchers - recipients
         s = "[#{issue.project.name} - #{issue.tracker.name} ##{issue.id}] "
         s << "(#{issue.status.name}) " if journal.new_value_for('status_id')
         s << issue.subject
@@ -50,60 +51,6 @@ module RedmineHelpdesk
           :cc => cc,
           :subject => s
         )
-      end
-      
-      # Sending email notifications to the supportclient
-      def email_to_supportclient(issue, sender_email, journal=nil, text='')
-        redmine_headers 'Project' => issue.project.identifier,
-                        'Issue-Id' => issue.id,
-                        'Issue-Author' => issue.author.login
-        redmine_headers 'Issue-Assignee' => issue.assigned_to.login if issue.assigned_to
-        message_id issue
-        recipients = sender_email
-        subject = "[#{issue.project.name} - #{issue.tracker.name} ##{issue.id}] (#{issue.status.name}) #{issue.subject}"
-        # Set 'from' email-address to 'helpdesk-sender-email' if available.
-        # Falls back to regular redmine behaviour if 'sender' is empty.
-        p = issue.project
-        s = CustomField.find_by_name('helpdesk-sender-email')
-        if !p.nil? && !s.nil?
-          sender = p.custom_value_for(s).try(:value)
-          from sender unless sender.blank?
-        end
-        # If a custom field with text for the first reply is
-        # available then use this one instead of the regular
-        r = CustomField.find_by_name('helpdesk-first-reply')
-        f = CustomField.find_by_name('helpdesk-email-footer')
-        reply  = p.nil? || r.nil? ? '' : p.custom_value_for(r).try(:value)
-        footer = p.nil? || f.nil? ? '' : p.custom_value_for(f).try(:value)
-        if text.present?
-          # sending out the journal note to the support client
-          mail(
-            :to      => recipients,
-            :subject => subject,
-            :body    => "#{text}\n\n#{footer}".gsub("##issue-id##", issue.id.to_s),
-            :date    => Time.zone.now
-          )
-        elsif reply.present?
-          # sending out the first reply message
-          mail(
-            :to      => recipients,
-            :subject => subject,
-            :body    => "#{reply}\n\n#{footer}".gsub("##issue-id##", issue.id.to_s),
-            :date    => Time.zone.now
-          )
-        else
-          # fallback on regular notification
-          @issue = issue
-          @journal = journal
-          @issue_url = url_for(:controller => 'issues', :action => 'show', :id => issue)
-          mail(
-            :to      => recipients,
-            :subject => subject,
-            :date    => Time.zone.now,
-            :template_path => 'mailer',
-            :template_name => 'issue_edit'
-          )
-        end
       end
       
     end # module InstanceMethods
