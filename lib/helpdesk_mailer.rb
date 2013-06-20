5f70dcfb667e8165ec958a3ec76164c618723687
jfqd <jfqd@blun.org>
2013-06-20 17:17:43 +0200

    need to set default_url_options for ActionMailer subclass

--- lib/helpdesk_mailer.rb
+++ lib/helpdesk_mailer.rb
@@ -4,6 +4,11 @@
 # why we need our own Mailer class.
 #
 class HelpdeskMailer < ActionMailer::Base
+  # set the hostname for url_for helper
+  def self.default_url_options
+    { :host => Setting.host_name, :protocol => Setting.protocol }
+  end
+  
   # Sending email notifications to the supportclient
   def email_to_supportclient(issue, recipient, journal=nil, text='')
     redmine_headers 'Project' => issue.project.identifier,
