d4ab5c3d158f88de19ef461d7716179cd7bdac36
Vilppu Vuorinen <vilppu.vuorinen@jubic.fi>
2015-03-16 10:26:40 +0200

    Refactored email_to_supportclient

--- lib/mailer_patch.rb
+++ lib/mailer_patch.rb
@@ -44,27 +44,16 @@ module RedmineHelpdesk
         end
       end
 
-
       # Sending email notifications to the supportclient
       def email_to_supportclient(issue, recipient, journal=nil, text='')
         redmine_headers 'Project' => issue.project.identifier,
                         'Issue-Id' => issue.id,
                         'Issue-Author' => issue.author.login
-        redmine_headers 'Issue-Assignee' => issue.assigned_to.login if issue.assigned_to
+        redmine_headers 'Issue-Assignee' => issue.
+            assigned_to.login if issue.assigned_to
         message_id issue
         references issue
-        subject = "[#{issue.project.name} - #{issue.tracker.name} ##{issue.id}] (#{issue.status.name}) #{issue.subject}"
-        # Set 'from' email-address to 'helpdesk-sender-email' if available.
-        # Falls back to regular redmine behaviour if 'sender' is empty.
-        p = issue.project
-        s = CustomField.find_by_name('helpdesk-sender-email')
-        sender = p.custom_value_for(s).try(:value) if p.present? && s.present?
-        # If a custom field with text for the first reply is
-        # available then use this one instead of the regular
-        r = CustomField.find_by_name('helpdesk-first-reply')
-        f = CustomField.find_by_name('helpdesk-email-footer')
-        reply  = p.nil? || r.nil? ? '' : p.custom_value_for(r).try(:value)
-        footer = p.nil? || f.nil? ? '' : p.custom_value_for(f).try(:value)
+
         # add any attachements
         if journal.present? && text.present?
           journal.details.each do |d|
@@ -78,56 +67,83 @@ module RedmineHelpdesk
             end
           end
         end
+        # create mail object to deliver
+
+        @issue = issue
+        @journal = journal
+        @issue_url = url_for(:controller => 'issues', :action => 'show', :id => issue)
+
+        __mail__(
+          helpdesk_headers(issue, recipient, text)
+        )
+      end
+
+      private
+
+      def helpdesk_headers(issue, recipient, text)
+        from = helpdesk_from(issue)
+        reply = helpdesk_reply(issue)
+        if text.present?
+          # sending out the journal note to the support client
+          body = "#{text}\n\n#{helpdesk_footer(issue)}"
+        elsif reply.present?
+          # sending out the first reply message
+          body = "#{reply}\n\n#{helpdesk_footer(issue)}"
+        end
+
+        h = {
+          :from     => from,
+          :reply_to => from,
+          :to       => recipient,
+          :subject  => helpdesk_subject(issue),
+          :date     => Time.zone.now
+        }
+
+        if body.empty?
+          h[:template_path] = 'mailer'
+          h[:template_name] = 'issue_edit'
+        else
+          h[:body] = body.gsub("##issue-id##", issue.id.to_s)
+        end
+        h
+      end
+
+      def helpdesk_subject(issue)
+        "[#{issue.project.name} - #{issue.tracker.name} ##{issue.id}] (#{issue.status.name}) #{issue.subject}"
+      end
+
+      def helpdesk_from(issue)
+        # Set 'from' email-address to 'helpdesk-sender-email' if available.
+        # Falls back to regular redmine behaviour if 'sender' is empty.
+        p = issue.project
+        s = CustomField.find_by_name('helpdesk-sender-email')
+        sender = p.custom_value_for(s).try(:value) if p.present? && s.present?
+
+        sender.present? && sender || Setting.mail_from
+      end
+
+      def helpdesk_reply(issue)
+        p = issue.project
+        r = CustomField.find_by_name('helpdesk-first-reply')
+        p.nil? || r.nil? ? '' : p.custom_value_for(r).try(:value)
+      end
+
+      def helpdesk_footer(issue)
+        p = issue.project
+        f = CustomField.find_by_name('helpdesk-email-footer')
+        p.nil? || f.nil? ? '' : p.custom_value_for(f).try(:value)
+      end
+
+      def __mail__(headers)
         if @message_id_object
           headers[:message_id] = "<#{self.class.message_id_for(@message_id_object)}>"
         end
         if @references_objects
           headers[:references] = @references_objects.collect {|o| "<#{self.class.references_for(o)}>"}.join(' ')
         end
-        # create mail object to deliver
-        mail = if text.present?
-          # sending out the journal note to the support client
-          __mail__(
-            :from     => sender.present? && sender || Setting.mail_from,
-            :reply_to => sender.present? && sender || Setting.mail_from,
-            :to       => recipient,
-            :subject  => subject,
-            :body     => "#{text}\n\n#{footer}".gsub("##issue-id##", issue.id.to_s),
-            :date     => Time.zone.now
-          )
-        elsif reply.present?
-          # sending out the first reply message
-          __mail__(
-            :from     => sender.present? && sender || Setting.mail_from,
-            :reply_to => sender.present? && sender || Setting.mail_from,
-            :to       => recipient,
-            :subject  => subject,
-            :body     => "#{reply}\n\n#{footer}".gsub("##issue-id##", issue.id.to_s),
-            :date     => Time.zone.now
-          )
-        else
-          # fallback to a regular notifications email with redmine view
-          @issue = issue
-          @journal = journal
-          @issue_url = url_for(:controller => 'issues', :action => 'show', :id => issue)
-          __mail__(
-            :from     => sender.present? && sender || Setting.mail_from,
-            :reply_to => sender.present? && sender || Setting.mail_from,
-            :to       => recipient,
-            :subject  => subject,
-            :date     => Time.zone.now,
-            :template_path => 'mailer',
-            :template_name => 'issue_edit'
-          )
-        end
-        # return mail object to deliver it
-        return mail
-      end
 
-      def __mail__(headers={}, &block)
-        ActionMailer::Base.instance_method(:mail).bind(self).call(headers, &block)
+        ActionMailer::Base.instance_method(:mail).bind(self).call(headers)
       end
-      private :__mail__
     end # module InstanceMethods
   end # module MailerPatch
 end # module RedmineHelpdesk
