f48767bac87392d7c9ea059a9b099a03be405664
Luca Pisani <luca.pisani@abstract.it>
2015-08-31 11:25:04 +0000

    Fix for working with private Projects - Fix For jfqd#75

--- lib/mail_handler_patch.rb
+++ lib/mail_handler_patch.rb
@@ -18,7 +18,7 @@ module RedmineHelpdesk
         roles = issue.author.roles_for_project(issue.project)
         # add owner-email only if the author has assigned some role with
         # permission treat_user_as_supportclient enabled
-        if roles.any? {|role| role.allowed_to?(:treat_user_as_supportclient) }
+        if issue.author.type.eql?("AnonymousUser") || roles.any? {|role| role.allowed_to?(:treat_user_as_supportclient) }
           sender_email = @email.from.first
           email_details = "From: " + @email[:from].formatted.first + "\n"
           email_details << "To: " + @email[:to].formatted.join(', ') + "\n"
