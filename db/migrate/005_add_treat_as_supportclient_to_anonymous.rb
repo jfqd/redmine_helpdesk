4049aa8f71e0833a8c8aaef2d961fe6e04deaff4
jfqd <jfqd@blun.org>
2019-10-30 11:38:35 +0100

    migration was still failing, fixed now?

--- db/migrate/005_add_treat_as_supportclient_to_anonymous.rb
+++ db/migrate/005_add_treat_as_supportclient_to_anonymous.rb
@@ -1,7 +1,7 @@
 class AddTreatAsSupportclientToAnonymous < ActiveRecord::Migration[5.2]
   def self.up
-    anon = User.where(type: 'AnonymousUser').first
-    Role.where(builtin: anon.id).add_permission!(:treat_user_as_supportclient)
+    anon_id = User.where(type: 'AnonymousUser').first.try(:id) || User.where('lastname LIKE ?', 'Anonymous').first.try(:id) || 4
+    Role.where(builtin: anon_id).add_permission!(:treat_user_as_supportclient)
   end
 
   def self.down
