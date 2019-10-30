b75de303e5154a230a28815eb16c69e2a539fb69
jfqd <jfqd@blun.org>
2019-10-30 11:22:04 +0100

    find user by type

--- db/migrate/005_add_treat_as_supportclient_to_anonymous.rb
+++ db/migrate/005_add_treat_as_supportclient_to_anonymous.rb
@@ -1,6 +1,6 @@
 class AddTreatAsSupportclientToAnonymous < ActiveRecord::Migration[5.2]
   def self.up
-    anon = User.where(lastname: 'Anonymous').first
+    anon = User.where(type: 'AnonymousUser').first
     Role.where(builtin: anon.id).add_permission!(:treat_user_as_supportclient)
   end
 
